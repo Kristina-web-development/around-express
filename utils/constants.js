1529615dbb259887e53ba6e6d43da557ef358285
Kristina Stasevich <3969626@gmail.com>
2022-09-22 23:58:18 +0300

    module exports

--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,5 @@
-export const NOT_FOUND = 404;
-export const SERVER_ERROR = 500;
-export const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
+const SERVER_ERROR = 500;
+const BAD_REQUEST = 400;
+
+module.exports = ({ NOT_FOUND, SERVER_ERROR, BAD_REQUEST });
